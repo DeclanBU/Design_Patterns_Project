265da420df4f1fe1bca58bf88103875f1efa36a9
Declan Buckley <declan.g.buckley@students.ittralee.ie>
2018-10-30 14:13:37 +0000

    Added emergencyBackupVehicleFactory,  high speed Porche and bike classes along with RoaduserVehicleFactory

---
+++ src/Factory/HighSpeedHondaBike.java
@@ -0,0 +1,12 @@
+package Factory;
+
+/**
+ * Created by Declan on 30/10/2018.
+ */
+public class HighSpeedHondaBike extends Vehicle {
+
+    public HighSpeedHondaBike(){
+
+        this.setType("High Speed Honda Bike");
+    }
+}
