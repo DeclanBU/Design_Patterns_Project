eef0ca1165b084df4e3ef2426cc882c34e8ae789
Declan Buckley <declan.g.buckley@students.ittralee.ie>
2018-11-29 21:09:54 +0000

    I used java Java's built in Observer Pattern as it's the last night before submission and i have more to do the document :)

---
+++ src/observer/Driver.java
@@ -0,0 +1,28 @@
+package observer;
+
+import java.util.Observable;
+
+/**
+ * Created by Declan on 29/11/2018.
+ */
+public class Driver extends Observable {
+
+        public void approachWindowHatch(){
+            System.out.println("Approaching order window");
+            setChanged();
+            notifyObservers(Moving.DRIVEUPTO);
+        }
+
+        public void orderFood(){
+            System.out.println("Order a big mac meal.");
+            setChanged();
+            notifyObservers(Moving.COLLECTFOOD);
+        }
+
+        public void foodCollected(){
+            System.out.println("Leave with food");
+            setChanged();
+            notifyObservers(Moving.LEAVE);
+        }
+    }
+
---
+++ src/observer/MacDonalds.java
@@ -0,0 +1,9 @@
+package observer;
+
+/**
+ * Created by Declan on 29/11/2018.
+ */
+public enum MacDonalds {
+    SERVERBUSY,ORDER;
+
+}
